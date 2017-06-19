e92b8ccd706e674f0abfa77f579e488481ee2a0f
Etienne Houze <Etienne.Houze@polytechnique.edu>
2017-06-19 10:19:44 +0200

    Evalutipon 3D (WIP)

--- Python/scripts/evaluate3D.py
+++ Python/scripts/evaluate3D.py
@@ -12,9 +12,16 @@ Syntaxe :
 """
 from __future__ import absolute_import, print_function, division
 
+import csv
 import getopt
-from os.path import join, isfile
+from os.path import join, isfile, isdir
 from os import mkdir
+
+import time
+
+from helpers.preprocess import dir_size
+from helpers.BatchGenerator import BatchGenerator
+from src.CityScapeModel import CityScapeModel
 import sys
 
 def main():
@@ -24,7 +31,80 @@ def main():
         print(msg)
         print(__doc__)
         sys.exit(-1)
-#         TODO : finir d'écrire ce gentil petit script...
+
+
+    all_metrics = False
+
+    # TODO : trouver comment mettre en place la visualisation
+    visu_folder = None
+
+    for o,a in opts:
+        if o in ['-h','--help']:
+            print(__doc__)
+            sys.exit(1)
+        if o in ['-a','--all']:
+            all_metrics = True
+        if o in ['-v','--visualize']:
+            visu_folder = a
+
+    model_folder = args[0]
+    validation_folder = args[1]
+    output_folder = args[2]
+    model = CityScapeModel(model_folder)
+
+    if not isdir(output_folder):
+        mkdir(output_folder)
+
+    if all_metrics:
+        mets = ['acc','iou']
+        for i in range(model.prop_dict['num_labs']):
+            mets.append('cat-iou_'+str(i))
+        model.define_metrics(*mets)
+
+    testdirsize = dir_size(join(validation_folder,'RGB'))
+    test_gen = BatchGenerator(traindir=validation_folder,
+                              traindirsize=testdirsize,
+                              trainsetsize=testdirsize,
+                              batchsize=1)
+    gen = test_gen.generate_batch_for_3D()
+    counter = 0
+
+    model.compile()
+    model.load_weights()
+    out_dict_list = []
+    for (x_test, y_test) in gen:
+        if counter < testdirsize:
+            line = {}
+            begin_time = time.time()
+            values = model.model.test_on_batch(x_test,y_test)
+            end_time = time.time()
+            line['loss'] = values[0]
+            line['time'] = end_time - begin_time
+            for i in range(1,len(values)):
+                line[model.prop_dict['metrics'][i-1]] = values[i]
+            print(line)
+            out_dict_list.append(line)
+            counter += 1
+        else:
+            break
+    with open(join(output_folder,'raw_output.csv'),'w') as f:
+        writer = csv.DictWriter(f, out_dict_list[0].keys())
+        writer.writeheader()
+        writer.writerows(out_dict_list)
+    means = out_dict_list[0]
+    for key in means.keys():
+        for i in range(1,testdirsize):
+            means[key] += out_dict_list[i][key]
+        means[key] /= testdirsize
+    with open(join(output_folder,'mean_output.csv'),'w') as f:
+        writer = csv.DictWriter(f, means.keys())
+        writer.writeheader()
+        writer.writerow(means)
+
+    # if visu_folder:
+
+
+
 
 if __name__ == '__main__':
     main()
